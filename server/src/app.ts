691418c5736740b5901a929d688bb61301c5f8fb
subc3tra <matmag04410@stud.noroff.no>
2026-06-05 11:14:11 +0200

    feat: add health route

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 
+import healthRouter from './routes/health.route';
+
 const app = express();
 
 app.use(express.json());
@@ -8,5 +10,8 @@ app.use(cors({
   origin: 'http://localhost:5173',
   credentials: true
 }));
+
+// Routes
+app.use('/api', healthRouter);
 
 export default app;
